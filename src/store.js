b9324d78dd742c0bbebb4e8d427cf62bc5197f13
donqu <itsol>
2019-11-23 16:27:39 +0700

    first update

---
+++ src/store.js
@@ -0,0 +1,4 @@
+import { applyMiddleware, createStore } from 'redux';
+import { createLogger } from 'redux-logger';
+
+import createHistory  from 'history'
---
+++ store.js
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import React from 'react';
+import { ConnectedRouter } from 'react-router-dom';
+ReactDOM.render((
+    <Provider>
+
+    </Provider>
+))
